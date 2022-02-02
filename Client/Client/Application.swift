2644dd2e4167411f00943afe475b2f38924d900f
ivan <kulikovia.72@icloud.com>
2022-02-02 14:48:24 +0200

    rename CardTransitionClient to Client

--- Client/Client/Application.swift
+++ Client/Client/Application.swift
@@ -4,13 +4,11 @@ final class Application: UIApplication, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        let window = UIWindow()
         let rootViewController = PresentingViewController()
 
-        window.rootViewController = rootViewController
-        window.makeKeyAndVisible()
-
-        self.window = window
+        window = UIWindow()
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
 
         return true
     }
